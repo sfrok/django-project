1919d520e2d5e73305bb895b0c3ed0fa863abd9f
Ситэрай <s.t.100.rus.version@gmail.com>
2019-07-25 14:53:28 +0300

    Added template view

--- baseapp/views.py
+++ baseapp/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request): return HttpResponse("<html><body>Hello World!</body></html>")
+
+def home2(request):
+    return render(request, 'home2.html', {})
