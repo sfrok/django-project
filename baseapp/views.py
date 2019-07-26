46648904bea326d4cefed00b7fe7421524c79760
Sergey <sfrok99@gmail.com>
2019-07-26 14:56:56 +0300

    added to pipfile --dev: flake8 package

--- baseapp/views.py
+++ baseapp/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def home(request): return HttpResponse("<html><body>Hello World!</body></html>")
+def home(request):
+    return HttpResponse("<html><body>Hello World!</body></html>")
 
 
 def home2(request):
