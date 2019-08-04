61b9c941aed12a9feb9e59deed2ff468740b329e
Sergey <sfrok99@gmail.com>
2019-08-04 13:44:34 +0300

    Rebasing static files

--- store/settings.py
+++ store/settings.py
@@ -170,5 +170,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+# The file storage engine to use when collecting static files with the collectstatic management command.
 
-STATIC_URL = '/baseapp/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'baseapp/static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'baseapp/media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'baseapp/static_dev'),
+    os.path.join(BASE_DIR, 'baseapp/media')
+)
--- store/urls.py
+++ store/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 import baseapp.views as v
@@ -22,3 +24,10 @@ urlpatterns = [
     path('home2/', v.home2, name='home2'),
     path('', v.home, name='home')
 ]
+if settings.DEBUG:
+    """
+    only serves the actual STATIC_ROOT folder
+    """
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
