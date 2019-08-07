72f2bd21c9c83d240570205d5f671e98803f4e55
Sergey <sfrok99@gmail.com>
2019-08-07 13:19:42 +0300

    import fix

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, AbstractUser,
-    PermissionsMixin)
+    BaseUserManager, AbstractBaseUser, AbstractUser)
 
 
 class MyUserManager(BaseUserManager):
