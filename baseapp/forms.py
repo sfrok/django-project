03ddfbfe0ee47255c39ff4c43e4019477384e356
Sergey <sfrok99@gmail.com>
2019-08-07 13:07:46 +0300

    Created authentification system

--- baseapp/forms.py
+++ baseapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from django.contrib.auth.models import User
+from .models import User
 
 
 class RegistrationForm(ModelForm):
--- baseapp/models.py
+++ baseapp/models.py
@@ -1,15 +1,88 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, AbstractUser,
+    PermissionsMixin)
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
-    a_country = models.CharField(max_length=128, blank=True)
-    a_city = models.CharField(max_length=128, blank=True)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    a_country = models.CharField(max_length=128, blank=True, null=True)
+    a_city = models.CharField(max_length=128, blank=True, null=True)
+    a_address = models.CharField(max_length=128, blank=True, null=True)
+    post_index = models.IntegerField(default=0)
+    payment_info = models.CharField(max_length=128, blank=True, null=True)
+    phone_number = models.CharField(max_length=16, blank=True, null=True)
+    objects = MyUserManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -26,6 +99,9 @@ class Product(models.Model):
     photo = models.ImageField()
     post_date = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         constraints = [
             models.CheckConstraint(check=models.Q(price__gte=0), name='price0'),
@@ -38,3 +114,13 @@ class Product(models.Model):
         ]
 
 
+class PersonalDiscount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    expires = models.DateTimeField
+    name = models.CharField(max_length=64)
+    value = models.IntegerField(default=0)
+    amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
