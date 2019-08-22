2ff7c165b95d1b83b294020cdefc5dc0029fc813
Ситэрай <s.t.100.rus.version@gmail.com>
2019-08-22 14:13:34 +0300

    Fixed syntax

--- store/settings.py
+++ store/settings.py
@@ -71,29 +71,29 @@ LOGGING = {
             'class': 'logging.StreamHandler',
             'formatter': 'simple'
         },
-        """'logstash': {
-            'level': 'INFO',
-            'class': 'logstash.TCPLogstashHandler',
-            'host': 'localhost',
-            'port': 5959,  # Default value: 5959
-            'version': 1,  # Version of logstash event schema.
-            # Default value: 0 (for backward compatibility of the library)
-            'message_type': 'django',  # 'type' field in logstash message. Default value: 'logstash'.
-            'fqdn': False,  # Fully qualified domain name. Default value: false.
-            'tags': ['django.request'],  # list of tags. Default: None.
-        },"""
+        #'logstash': {
+            #'level': 'INFO',
+            #'class': 'logstash.TCPLogstashHandler',
+            #'host': 'localhost',
+            #'port': 5959,  # Default value: 5959
+            #'version': 1,  # Version of logstash event schema.
+            ## Default value: 0 (for backward compatibility of the library)
+            #'message_type': 'django',  # 'type' field in logstash message. Default value: 'logstash'.
+            #'fqdn': False,  # Fully qualified domain name. Default value: false.
+            #'tags': ['django.request'],  # list of tags. Default: None.
+        #},
       }
-  """'loggers': {
-        'django.request': {
-            'handlers': ['logstash'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'django': {
-            'handlers': ['console'],
-            'propagate': True,
-        },
-    }"""
+  #'loggers': {
+        #'django.request': {
+            #'handlers': ['logstash'],
+            #'level': 'INFO',
+            #'propagate': True,
+        #},
+        #'django': {
+            #'handlers': ['console'],
+            #'propagate': True,
+        #},
+    #}
 }
 
 MIDDLEWARE = [
