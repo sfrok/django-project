37b576aec4c956eef999159b1c4c3ba8db9ce28f
Ситэрай <s.t.100.rus.version@gmail.com>
2019-08-26 12:23:25 +0300

    Docs refactor and Search cat filter prototype

--- baseapp/search.py
+++ baseapp/search.py
@@ -1,10 +1,13 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import Q
 
 
-def search(line):
+def search(line, cat = None):
     client = Elasticsearch()
-    s = Search(using=client, index="products").query("match", name=line)
+    if cat == None: s = Search(using=client, index="products").query("match", name=line)
+    else: s = Search(using=client, index="products").query(
+        Q('match', name=line) & Q('contains', categories=cat))
     response = s.execute()
 
     return [hit.name for hit in response]
