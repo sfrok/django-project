ea7e8bfdb1cf74ea4ca3a838716add5585a0710a
Ситэрай <s.t.100.rus.version@gmail.com>
2019-09-15 12:21:52 +0300

    Search and query adjustments

--- baseapp/search.py
+++ baseapp/search.py
@@ -8,29 +8,41 @@ from django.utils import six
 def search(line, cat=None, sort_attr=None):
     client = Elasticsearch()
     if cat is None:
-        s = Search(using=client, index="products") \
-            # .query("match_phrase", name=line)  # \
-        # .sort('name' if sort_attr is None else sort_attr)
+        if line != '':
+            s = Search(using=client, index="products") \
+                .query("match_phrase", name=line)  # \
+                # .sort('name' if sort_attr is None else sort_attr)
+        else:
+            s = Search(using=client, index="products")
     else:
         queries = []
         for i in cat:
             queries.append(Q('match', category=i))
-        s = Search(using=client, index="products") \
-            .query(Q('match_phrase', name=line) &
-                   Q(six.moves.reduce(ior, queries)))  # \
-        # .sort('name' if sort_attr is None else sort_attr)
+        if line != '':
+            s = Search(using=client, index="products") \
+                .query(Q('match_phrase', name=line) &
+                    Q(six.moves.reduce(ior, queries)))
+        else:
+            s = Search(using=client, index="products") \
+                .query(Q(six.moves.reduce(ior, queries)))
     response = s.execute()
     response = sorted(response, key=lambda k: k['name' if sort_attr is None else sort_attr])
     return [hit for hit in response]
 
 
-def query(i, line, attr=None, match_attr=None, sort_attr=None):
+def query(i, line=None, attr=None, match_attr=None, sort_attr=None):
     client = Elasticsearch()
-    s = Search(using=client, index=i) \
-        .query("match_phrase" if match_attr is None else match_attr, \
-               **{('name' if attr is None else attr): line})  # \
-    # .sort(('name' if attr is None else attr) if sort_attr is None else sort_attr)
+    if line is not None:
+        s = Search(using=client, index=i) \
+            .query("match_phrase" if match_attr is None else match_attr, \
+                **{('name' if attr is None else attr): line})  # \
+            # .sort(('name' if attr is None else attr) if sort_attr is None else sort_attr)
+    else:
+        s = Search(using=client, index=i)
+    
     response = s.execute()
+    response = sorted(response, key=lambda k: \
+        k[('name' if attr is None else attr) if sort_attr is None else sort_attr])
     return [vars(hit) for hit in response]
 
 
