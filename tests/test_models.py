3a000aa3a9a96379f5de5f8d6511f4cc05fed4e0
Ситэрай <s.t.100.rus.version@gmail.com>
2019-10-05 23:54:34 +0300

    Model test prototype 1

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,15 @@
 from baseapp.models import Product
 
-for i in range(10):
-    Product(id = i+1, name='Товар '+str(i+1), price=i**2*1000, amount = 100-i*10).save()
+
+def test_models1():
+    tmp = Product.objects.all()
+    a = []
+    for i in range(10):
+        a.append(Product(id = i+1, name='Товар '+str(i+1), price=-1*i**2*1000, amount = 100-i*10))
+        a[i].save()
+        assert Product.objects.get(id=a[i].id), f"data failure, id={a[i].id}"
+    assert Product.objects.get(id=999) != None, f"id failure, result exists"
+
+    # Stashing changes
+    Product.objects.clear()
+    tmp.save()
