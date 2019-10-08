d5080a1623a405a2ae16eb6eea3fb747269f25ce
Sergey <sfrok99@gmail.com>
2019-10-08 20:58:37 +0300

    added test of the registration system

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pytest
 import logging
-import time
 
+from baseapp.models import User
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,12 +14,17 @@ class TestClassRegAuthSystem:
     def test_registration_form_check(self):
         TestClassRegAuthSystem.driver.get("http://127.0.0.1:9012/registration/")
         all_inputs = TestClassRegAuthSystem.driver.find_elements_by_xpath('//form[@id = "registration"]//input')
+        before = User.objects.all()
         for input in all_inputs[1:]:
             input.send_keys('$$$$')
         TestClassRegAuthSystem.driver.find_element_by_id('submit_btn').click()
-        time.sleep(6)
-        # assert driver.switch_to_alert() not in driver.page_source
-
+        after = User.objects.all()
+        assert len(before) == len(after), f'failed, before = {len(before)}, after = {len(after)}'
+        for i in range(len(before)):
+            assert before[i] == after[i], f'failed, i = {i}, before = {before[i]}, after = {after[i]}'
+        User.objects.all().delete()
+        for i in before:
+            i.save()
     # def test_authorization_form_check(self):
     #     driver.get("http://127.0.0.1:8090/auth/")
 
