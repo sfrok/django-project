7374315d8507cbbdd095694770a525342ef9d9bb
Ситэрай <s.t.100.rus.version@gmail.com>
2019-10-12 01:21:41 +0300

    Another test refactor, flake8

--- baseapp/tests/selenium_test.py
+++ baseapp/tests/selenium_test.py
@@ -1,23 +1,20 @@
 from django.test import TestCase
 from baseapp.models import User
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
 
-class TestClassRegAuthSystem(TestCase):
+class TestRegAuth(TestCase):
     driver = webdriver.Chrome()
 
     def test_registration_form_check(self):
-        TestClassRegAuthSystem.driver.get("http://127.0.0.1:9012/registration/")
-        all_inputs = TestClassRegAuthSystem.driver.find_elements_by_xpath('//form[@id = "registration"]//input')
+        TestRegAuth.driver.get("http://127.0.0.1:9012/registration/")
+        all_inputs = TestRegAuth.driver.find_elements_by_xpath(
+            '//form[@id = "registration"]//input')
         for input in all_inputs[1:]:
             input.send_keys('$$$$')
-        TestClassRegAuthSystem.driver.find_element_by_id('submit_btn').click()
+        TestRegAuth.driver.find_element_by_id('submit_btn').click()
         u = User.objects.all()
         self.assertFalse(u.exists(), f'failed, len = {len(u)}')
-
-    # def test_authorization_form_check(self):
-    #     driver.get("http://127.0.0.1:8090/auth/")
--- baseapp/tests/test_models.py
+++ baseapp/tests/test_models.py
@@ -6,16 +6,16 @@ import baseapp.search as func
 class TestDB(TestCase):
     @classmethod
     def setUpTestData(cls):
-        # Set up data for the whole TestCase, DO NOT MODIFY THIS DATA OUTSIDE SETUP!
+        # DO NOT MODIFY THIS DATA OUTSIDE SETUP!
         cls.data = []
         for i in range(10):
-            cls.data.append(Product(id=i+1, 
-                                    name='Товар '+str(i+1), 
-                                    price=i**2*1000, 
-                                    amount=100-i*10))
+            cls.data.append(Product(
+                id=i + 1,
+                name='Товар ' + str(i + 1),
+                price=i ** 2 * 1000,
+                amount=100 - i * 10))
 
     def test_model_product(self):
-        a = []
         for i in self.data:
             i.save()
             self.assertTrue(Product.objects.filter(id=i.id).exists(), f"data failure, id={i.id}")
@@ -23,5 +23,5 @@ class TestDB(TestCase):
 
     def test_search_line(self):
         f = func.search('Товар 3')
-        self.assertTrue (f[0]['name'] == 'Товар 3', 
-                         f"results aren't equal:\nES: {f}\n f[0]['name']: {f[0]['name']}")
+        self.assertTrue(f[0]['name'] == 'Товар 3', 
+                        f"results aren't equal:\nES: {f}\n f[0]['name']: {f[0]['name']}")
