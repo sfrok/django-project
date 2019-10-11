bdc944058a44d2feeddb1fe8255a66ec9ba937de
Ситэрай <s.t.100.rus.version@gmail.com>
2019-10-11 13:47:20 +0300

    Test refactor using flake8, added test data setup

--- baseapp/tests/test_models.py
+++ baseapp/tests/test_models.py
@@ -1,13 +1,27 @@
 from django.test import TestCase
 from baseapp.models import Product
+import baseapp.search as func
 
 
-class TestModels(TestCase):
-
-    def test_models1(self):
-        a = []
+class TestDB(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase, DO NOT MODIFY THIS DATA OUTSIDE SETUP!
+        cls.data = []
         for i in range(10):
-            a.append(Product(id=i + 1, name='Товар ' + str(i + 1), price=1 * i ** 2 * 1000, amount=100 - i * 10))
-            a[i].save()
-            self.assertTrue(Product.objects.filter(id=a[i].id).exists(), f"data failure, id={a[i].id}")
+            cls.data.append(Product(id=i+1, 
+                                    name='Товар '+str(i+1), 
+                                    price=i**2*1000, 
+                                    amount=100-i*10))
+
+    def test_model_product(self):
+        a = []
+        for i in self.data:
+            i.save()
+            self.assertTrue(Product.objects.filter(id=i.id).exists(), f"data failure, id={i.id}")
         self.assertFalse(Product.objects.filter(id=999).exists(), f"id failure, result exists")
+
+    def test_search_line(self):
+        f = func.search('Товар 3')
+        self.assertTrue (f[0]['name'] == 'Товар 3', 
+                         f"results aren't equal:\nES: {f}\n f[0]['name']: {f[0]['name']}")
--- baseapp/tests/test_search.py
+++
@@ -1,6 +0,0 @@
-import baseapp.search as func
-
-
-def test_search():
-    f = func.search("Train", cat=['tech', 'food'])
-    assert type(f) is list and len(f) > 0
