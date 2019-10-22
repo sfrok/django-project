1002b71c10a7fa8ad080c02b0807bb8d40fd8caf
Sergey <sfrok99@gmail.com>
2019-10-22 21:10:20 +0300

    Overlay for order-form v0.1

--- baseapp/static_dev/ba-js/order-form.js
+++ baseapp/static_dev/ba-js/order-form.js
@@ -1,5 +1,6 @@
 makeOrderBtn = document.querySelector('#make-order');
 quitWithFormBtn = document.querySelector('#modal-quit');
+orderEffect = document.querySelector('#order-effect');
 
 makeOrderBtn.onclick = modalWindowShow;
 quitWithFormBtn.onclick = modalWindowQuit;
@@ -8,8 +9,14 @@ formInstance = document.querySelector('#modal-form');
 
 function modalWindowShow() {
     formInstance.style.display = 'block';
+    orderEffect.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    orderEffect.style.position = 'absolute';
+    orderEffect.style.width = '100%';
+    orderEffect.style.height = '100%';
+
 }
 
 function modalWindowQuit() {
     formInstance.style.display = 'none';
+    orderEffect.style.backgroundColor = '';
 }
