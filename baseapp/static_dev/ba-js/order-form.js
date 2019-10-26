a58077195e211d1d7f2399bd22f2d32d96cc7ef6
Sergey <sfrok99@gmail.com>
2019-10-26 12:26:10 +0300

    added overlay

--- baseapp/static_dev/ba-js/order-form.js
+++ baseapp/static_dev/ba-js/order-form.js
@@ -1,22 +1,18 @@
 makeOrderBtn = document.querySelector('#make-order');
 quitWithFormBtn = document.querySelector('#modal-quit');
-orderEffect = document.querySelector('#order-effect');
 
 makeOrderBtn.onclick = modalWindowShow;
 quitWithFormBtn.onclick = modalWindowQuit;
 
 formInstance = document.querySelector('#modal-form');
+overlay = document.querySelector('.hide');
 
 function modalWindowShow() {
     formInstance.style.display = 'block';
-    orderEffect.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    orderEffect.style.position = 'absolute';
-    orderEffect.style.width = '100%';
-    orderEffect.style.height = '100%';
-
+    overlay.classList.remove('hide')
 }
 
 function modalWindowQuit() {
+    overlay.classList.add('hide');
     formInstance.style.display = 'none';
-    orderEffect.style.backgroundColor = '';
 }
