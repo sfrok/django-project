81955d2f8d84b3d99cef739c398f4f06a1cae161
Sergey <sfrok99@gmail.com>
2019-10-27 16:58:22 +0200

    made new migrations

--- baseapp/admin.py
+++ baseapp/admin.py
@@ -1,10 +1,9 @@
 from .forms import UserAdmin
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Product, User, PersonalDiscount, Order, Status
+from .models import Product, User, PersonalDiscount, Order
 
 admin.site.register(Order)
-admin.site.register(Status)
 admin.site.register(Product)
 admin.site.register(PersonalDiscount)
 admin.site.register(User, UserAdmin)
