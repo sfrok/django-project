9f2340c85740730b97c8668aec478470aecc7934
Ситэрай <s.t.100.rus.version@gmail.com>
2019-12-19 01:31:54 +0200

    Bugfixes

--- store/settings.py
+++ store/settings.py
@@ -152,7 +152,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
+DATETIME_FORMAT = 'd.m.Y, G:i'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
