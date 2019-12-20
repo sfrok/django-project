7737ad169d2621f965cba9a89a2f9b279ba0d710
Ситэрай <s.t.100.rus.version@gmail.com>
2019-12-21 01:08:03 +0200

    Activation v1

--- baseapp/auth.py
+++ baseapp/auth.py
@@ -2,10 +2,16 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from store.data import HtmlPages
+from django.utils.translation import gettext as _
+from .models import User
 
 #  Для класса TokenGenerator
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
+
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
@@ -25,6 +31,14 @@ def auth(request, form, page):  # Главная ф-ия авторизации
             password = form.cleaned_data.get("password")
             if page == HtmlPages.reg:
                 user = form.save()  # Сохранение нового пользователя
+                subject = 'Activate your account.'
+                current_site = get_current_site(request)
+                uid = urlsafe_base64_encode(force_bytes(user.pk))
+                token = tokenGen.make_token(user)
+                link = "{0}/?uid={1}&token{2}".format(current_site, uid, token)
+                text = (_("Здравствуйте"), _("перейдите по этой ссылке для активации"))
+                message = "{0}, {1}, {2}:\n {3}".format(text[0], user.name, text[1], link)
+                user.email_user(subject, message)
             user = authenticate(email=email, password=password)
             if user:
                 login(request, user)
@@ -38,7 +52,14 @@ def deAuth(request):  # Главная ф-ия деавторизации
     return HttpResponseRedirect('/')
 
 
-def activate(request, uidb64=None, token=None):
-    request.user.is_active = True  # activate user
-    request.user.save()
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and tokenGen.check_token(user, token):
+        user.is_confirmed = True  # activate user
+        user.save()
+        login(request, user)
     return HttpResponseRedirect('/')
