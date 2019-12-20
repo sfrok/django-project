ab69df95ab9cfbd338f9ae31653d9acb222cb68b
Sergey <sfrok99@gmail.com>
2019-12-20 17:32:15 +0200

    full sidebar

--- baseapp/static_dev/ba-js/sidebar.js
+++ baseapp/static_dev/ba-js/sidebar.js
@@ -1,4 +1,14 @@
-
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('active');
+    document.querySelector('.overlay').style.display = 'block';
 }
+
+$(".sidebar-exit").click(function() {
+    document.getElementById('sidebar').classList.toggle('active');
+    document.querySelector('.overlay').style.display = 'none';
+});
+
+$(".overlay").click(function() {
+    document.getElementById('sidebar').classList.toggle('active');
+    document.querySelector('.overlay').style.display = 'none';
+});
