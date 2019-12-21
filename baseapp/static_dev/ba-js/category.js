d227be55d82f62512976690f957afeb8904e0de6
Sergey <sfrok99@gmail.com>
2019-12-21 16:48:53 +0200

    some ajax fixes v2.0

---
+++ baseapp/static_dev/ba-js/category.js
@@ -0,0 +1,13 @@
+$(".wrapper-img").click(function () {
+    let categoryid = parseInt($(".cart-title").attr("id"));
+    let csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+    $.ajax({
+       type: "POST",
+       url: "/products/",
+       data: {
+           categoryData:categoryid,
+           csrfmiddlewaretoken: csrftoken,
+       },
+       dataType: "json",
+    });
+});
