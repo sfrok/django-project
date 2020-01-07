11cdf37ee69126114bf511c21994a0323e9e60eb
Egor <marderosov7@gmail.com>
2020-01-07 13:17:22 +0200

    Fix form

--- baseapp/static_dev/ba-js/sidebar.js
+++ baseapp/static_dev/ba-js/sidebar.js
@@ -1,14 +1,14 @@
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('active');
-    document.querySelector('.overlay').style.display = 'block';
+    document.querySelector('.overlay-base').style.display = 'block';
 }
 
 $(".sidebar-exit").click(function() {
     document.getElementById('sidebar').classList.toggle('active');
-    document.querySelector('.overlay').style.display = 'none';
+    document.querySelector('.overlay-base').style.display = 'none';
 });
 
-$(".overlay").click(function() {
+$(".overlay-base").click(function() {
     document.getElementById('sidebar').classList.toggle('active');
-    document.querySelector('.overlay').style.display = 'none';
+    document.querySelector('.overlay-base').style.display = 'none';
 });
